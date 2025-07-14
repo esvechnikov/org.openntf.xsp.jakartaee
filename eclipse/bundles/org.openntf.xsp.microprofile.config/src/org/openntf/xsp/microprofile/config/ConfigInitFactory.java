06834c4f655e1bbf453fae001d605ed83f771e81
Svechnikov Evgeniy <evgeniy.svechnikov@evraz.com>
2025-07-14 11:00:16 +0500

    Fix for 2.x #662

--- eclipse/bundles/org.openntf.xsp.microprofile.config/src/org/openntf/xsp/microprofile/config/ConfigInitFactory.java
+++ eclipse/bundles/org.openntf.xsp.microprofile.config/src/org/openntf/xsp/microprofile/config/ConfigInitFactory.java
@@ -15,59 +15,23 @@
  */
 package org.openntf.xsp.microprofile.config;
 
-import java.security.AccessController;
-import java.security.PrivilegedAction;
-
 import org.eclipse.microprofile.config.spi.ConfigProviderResolver;
-import org.openntf.xsp.jakartaee.module.ComponentModuleLocator;
-import org.openntf.xsp.microprofile.config.sources.ImplicitAppConfigSourceFactory;
-import org.openntf.xsp.microprofile.config.sources.NotesEnvironmentConfigSource;
-import org.openntf.xsp.microprofile.config.sources.XspPropertiesConfigSourceFactory;
 
 import com.ibm.designer.runtime.domino.adapter.HttpService;
 import com.ibm.designer.runtime.domino.adapter.IServiceFactory;
 import com.ibm.designer.runtime.domino.adapter.LCDEnvironment;
 
-import io.smallrye.config.PropertiesLocationConfigSourceFactory;
-import io.smallrye.config.SmallRyeConfigBuilder;
 import io.smallrye.config.SmallRyeConfigProviderResolver;
-import io.smallrye.config.SysPropConfigSource;
-import jakarta.servlet.ServletContext;
 
 public class ConfigInitFactory implements IServiceFactory {
 
 	@Override
 	public HttpService[] getServices(LCDEnvironment env) {
-		SmallRyeConfigProviderResolver resolver = new SmallRyeConfigProviderResolver() {
-			@Override
-			public SmallRyeConfigBuilder getBuilder() {
-				SmallRyeConfigBuilder builder = super.getBuilder();
 
-				// Manually add sources that would come from ServiceLoader
-				builder.withSources(
-					new SysPropConfigSource(),
-					new NotesEnvironmentConfigSource()
-				);
-				builder.withSources(
-					new PropertiesLocationConfigSourceFactory(),
-					new XspPropertiesConfigSourceFactory(),
-					new ImplicitAppConfigSourceFactory()
-				);
-				
-				AccessController.doPrivileged((PrivilegedAction<Void>)() -> {
-					ComponentModuleLocator.getDefault()
-						.flatMap(ComponentModuleLocator::getServletContext)
-						.map(ServletContext::getClassLoader)
-						.ifPresent(builder::forClassLoader);
-					return null;
-				});
-				
-				return builder;
-			}
-		};
-		
+		SmallRyeConfigProviderResolver resolver = new JakartaConfigProviderResolver();
+
 		ConfigProviderResolver.setInstance(resolver);
-		
+
 		return null;
 	}
 
